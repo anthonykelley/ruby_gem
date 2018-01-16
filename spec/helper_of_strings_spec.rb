6fe87c3314fa5cd24c637f41ec5633d25ac3d987
Anthony Kelley <anthonymkelley@gmail.com>
2018-01-16 10:21:36 -0700

    update test

--- spec/helper_of_strings_spec.rb
+++ spec/helper_of_strings_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe HelperOfStrings do
+  subject { HelperOfStrings }
+
   it "has a version number" do
     expect(HelperOfStrings::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "manipulate strings" do
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y S t r i n g')
+    end
   end
 end
